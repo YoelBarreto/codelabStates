f1b5435084d07eae3a0e968b6b51af23ffbc26d0
Yoel <yobelb8@gmail.com>
2024-11-27 12:50:29 +0000

    Modificado error de que solo salta task #1

--- src/main/kotlin/WellnessTaskList.kt
+++ src/main/kotlin/WellnessTaskList.kt
@@ -3,22 +3,21 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalMapOf
 
 
 data class WellnessTask(val id: Int, val label: String)
 
-fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $1")}
+fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i")}
 
 
 @Composable
 fun WellnessTaskList(
-    modifier: Modifier = Modifier,
     listaItems: List<WellnessTask> = remember { getWellnessTasks() }
 ) {
-    LazyColumn(
+    LazyColumn (
         state = rememberLazyListState()
-    ) {
+    ){
         items(listaItems) {
             WellnessTaskItem(it.label, {})
         }
